15e5e3d2c99bc214343ec9ae8a05ee8e461b52bb
Vedran Bojanic <Vedran.Bojanic@extern.consorsbank.de>
2019-04-03 14:11:19 +0200

    Registration added

--- src/main/java/hr/brewer/configuration/SecurityConfig.java
+++ src/main/java/hr/brewer/configuration/SecurityConfig.java
@@ -13,13 +13,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
-                .authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll().anyRequest().authenticated()
-                .and().httpBasic();
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers(HttpMethod.OPTIONS, "/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
     }
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("javainuse").password("{noop}password").roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("USER");
     }
 }
--- src/main/java/hr/brewer/controllers/AuthenticationController.java
+++ src/main/java/hr/brewer/controllers/AuthenticationController.java
@@ -1,11 +1,9 @@
 package hr.brewer.controllers;
 
 import hr.brewer.models.User;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin()
 @RestController
 public class AuthenticationController {
 
